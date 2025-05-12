a6d4c3e726fbef9b3c61694a0969f1530f4b249d
Dany <dany.mahmalat@gmail.com>
2025-05-12 15:32:54 -0400

    Initial commit

--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"os"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
-	klog "k8s.io/klog/v2"
 
-	"github.com/mdonoughe/cert-manager-porkbun/porkbun"
+	"github.com/lost-woods/cert-manager-porkbun-webhook/porkbun"
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
 
 func main() {
 	if GroupName == "" {
-		klog.Fatal("GROUP_NAME must be specified")
+		panic("GROUP_NAME must be specified")
 	}
 
 	// This will register our custom DNS provider with the webhook serving
